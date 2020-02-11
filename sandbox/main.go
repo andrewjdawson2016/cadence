0999eb5ac3ff8fa940cd012919bc3396a0770302
Andrew Dawson <dandrew@uber.com>
2020-02-11 14:24:57 -0800

    add more to tally bench test

--- sandbox/main.go
+++ sandbox/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"runtime"
 	"time"
 
 	"github.com/uber-go/tally"
 	"github.com/uber-go/tally/m3"
+	_ "net/http/pprof"
 )
 
 func main() {
@@ -32,9 +35,28 @@ func main() {
 
 }
 
+func PrintMemUsage() {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	// For info on each, see: https://golang.org/pkg/runtime/#MemStats
+	fmt.Printf("Alloc = %v MiB", bToMb(m.Alloc))
+	fmt.Printf("\tTotalAlloc = %v MiB", bToMb(m.TotalAlloc))
+	fmt.Printf("\tSys = %v MiB", bToMb(m.Sys))
+	fmt.Printf("\tNumGC = %v\n", m.NumGC)
+}
+
 func emitMetrics(s tally.Scope, interval time.Duration) {
 	tic := time.NewTicker(interval)
+	i := 0
 	for range tic.C {
+		if i % 100 == 0 {
+			PrintMemUsage()
+		}
 		s.Tagged(map[string]string{"app": "codelab"}).Counter("example_counter").Inc(1)
+		i++
 	}
+}
+
+func bToMb(b uint64) uint64 {
+	return b / 1024 / 1024
 }
